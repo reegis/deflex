06d187297c52d3e8cc8d0951178ddf52c84fb000
uwe <uwe.krien@rl-institut.de>
2018-04-06 18:09:03 +0200

    extend setup.py to avoid installation errors

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 #! /usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
 
-setup(name='de21',
-      version='0.0.1',
-      author='Uwe Krien',
-      author_email='uwe.krien@rl-institut.de',
-      description='A reegis model of Germany with 21 region (18 +3).',
-      package_dir={'de21': 'de21'},
-      install_requires=['oemof >= 0.1.0',
-                        'pandas >= 0.17.0',
-                        'reegis_tools',
-                        'demandlib',
-                        'workalendar',
-                        'networkx',
-                        'numpy',
-                        'shapely']
-      )
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='de21',
+    version='0.0.1',
+    author='Uwe Krien',
+    author_email='uwe.krien@rl-institut.de',
+    description='A reegis model of Germany with 21 region (18 +3).',
+    namespace_package=['Open_eQuarterPy'],
+    long_description=read('README.rst'),
+    packages=find_packages(),
+    package_dir={'de21': 'de21'},
+    install_requires=[
+        'oemof >= 0.1.0',
+        'pandas >= 0.17.0',
+        'reegis_tools',
+        'demandlib',
+        'workalendar',
+        'networkx',
+        'numpy',
+        'shapely'])
