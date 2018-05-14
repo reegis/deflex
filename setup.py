571dec592320f1685b21edc8e157f5b26c93a122
uwe <uwe.krien@rl-institut.de>
2018-05-14 18:42:00 +0200

    adapt setup file

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@ def read(fname):
 
 
 setup(
-    name='de21',
+    name='deflex',
     version='0.0.1',
     author='Uwe Krien',
     author_email='uwe.krien@rl-institut.de',
-    description='A reegis model of Germany with 21 region (18 +3).',
-    namespace_package=['Open_eQuarterPy'],
+    description='A reegis model of Germany with flexible multi regions.',
+    namespace_package=['deflex'],
     long_description=read('README.rst'),
     packages=find_packages(),
-    package_dir={'de21': 'de21'},
+    package_dir={'deflex': 'deflex'},
     install_requires=[
         'oemof >= 0.1.0',
         'pandas >= 0.17.0',
