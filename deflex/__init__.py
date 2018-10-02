1053a76c73203dd99f21518c765caada9a3099e0
uwe <uvchik.git@posteo.eu>
2018-10-02 11:06:59 +0200

    Extend init file

--- deflex/__init__.py
+++ deflex/__init__.py
@@ -2,3 +2,5 @@ from deflex.scenario_tools import Scenario
 from deflex import chp
 from deflex import basic_scenario
 from deflex.main import main
+from deflex import demand
+from deflex import inhabitants
