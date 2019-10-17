ea7066a15da81154b73f166d60458e884a404248
uvchik <uvchik.git@posteo.eu>
2019-10-17 10:13:29 +0200

    Adapt licence

--- deflex/chp.py
+++ deflex/chp.py
@@ -2,12 +2,12 @@
 
 """Processing a list of power plants in Germany.
 
-Copyright (c) 2016-2018 Uwe Krien <uwe.krien@rl-institut.de>
+Copyright (c) 2016-2019 Uwe Krien <krien@uni-bremen.de>
 
-SPDX-License-Identifier: GPL-3.0-or-later
+SPDX-License-Identifier: MIT
 """
-__copyright__ = "Uwe Krien <uwe.krien@rl-institut.de>"
-__license__ = "GPLv3"
+__copyright__ = "Uwe Krien <krien@uni-bremen.de>"
+__license__ = "MIT"
 
 
 # Python libraries
--- deflex/main.py
+++ deflex/main.py
@@ -2,12 +2,12 @@
 
 """Main script.
 
-Copyright (c) 2016-2018 Uwe Krien <uwe.krien@rl-institut.de>
+Copyright (c) 2016-2019 Uwe Krien <krien@uni-bremen.de>
 
-SPDX-License-Identifier: GPL-3.0-or-later
+SPDX-License-Identifier: MIT
 """
-__copyright__ = "Uwe Krien <uwe.krien@rl-institut.de>"
-__license__ = "GPLv3"
+__copyright__ = "Uwe Krien <krien@uni-bremen.de>"
+__license__ = "MIT"
 
 
 # Python libraries
@@ -85,7 +85,7 @@ def main(year, plot_graph=False):
 if __name__ == "__main__":
     logger.define_logging()
     for y in [2014, 2013, 2012]:
-        for my_rmap in ['de21', 'de22']:
+        for my_rmap in ['de02', 'de17', 'de21']:
             cfg.tmp_set('init', 'map', my_rmap)
             try:
                 main(y)
