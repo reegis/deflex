199de569df0fabe95d1f16ce1ba867abf11bd438
uvchik <uvchik.git@posteo.eu>
2019-10-21 17:26:01 +0200

    Add version

--- deflex/__init__.py
+++ deflex/__init__.py
@@ -1,7 +1,1 @@
-from deflex.scenario_tools import Scenario
-from deflex import chp
-from deflex import basic_scenario
-from deflex.main import main
-from deflex import demand
-from deflex import inhabitants
-from deflex import geometries
+__version__ = '0.0.1'
