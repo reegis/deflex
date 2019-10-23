a856a3a4214bc725fe08ac85fff4f3d023ff07d5
uvchik <uvchik.git@posteo.eu>
2019-10-23 16:52:47 +0200

    Add Scenario tests

--- tests/test_scenario_tools.py
+++ tests/test_scenario_tools.py
@@ -9,7 +9,8 @@ SPDX-License-Identifier: MIT
 __copyright__ = "Uwe Krien <krien@uni-bremen.de>"
 __license__ = "MIT"
 
-from nose.tools import assert_raises_regexp
+from nose.tools import eq_, assert_raises_regexp
+import shutil
 import os
 from deflex import scenario_tools
 
@@ -30,3 +31,56 @@ def test_node_dict():
            "node dictionary.")
     with assert_raises_regexp(KeyError, msg):
         nc['g'] = 7
+
+
+def test_scenario_es_init():
+    sc = scenario_tools.DeflexScenario(name='test', year=2012, debug=True)
+    es1 = sc.initialise_energy_system()
+    sc = scenario_tools.DeflexScenario(name='test', year=2012)
+    es2 = sc.initialise_energy_system()
+    sc = scenario_tools.DeflexScenario(name='test', year=2013)
+    es3 = sc.initialise_energy_system()
+    eq_(len(es1.timeindex), 3)
+    eq_(len(es2.timeindex), 8784)
+    eq_(len(es3.timeindex), 8760)
+
+
+def test_scenario_es_init_error():
+    sc = scenario_tools.DeflexScenario()
+    msg = ("You cannot create an EnergySystem with self.year=2012, of type"
+           " <class 'str'")
+    with assert_raises_regexp(TypeError, msg):
+        sc.initialise_es('2012')
+
+
+def test_excel_reader():
+    sc = scenario_tools.DeflexScenario(name='test', year=2014)
+    xls_fn = os.path.join(
+        os.path.dirname(__file__), 'data', 'deflex_2013_de02_test.xls')
+    sc.load_excel(xls_fn)
+    sc.table2es()
+    csv_path = os.path.join(
+        os.path.expanduser('~'), 'deflex_2014_de02_nose_test_csv')
+    sc.to_csv(csv_path)
+    xls_fn = os.path.join(
+        os.path.expanduser('~'), 'deflex_2014_de02_nose_test.xls')
+    sc.to_excel(xls_fn)
+    shutil.rmtree(csv_path)
+    os.remove(xls_fn)
+
+
+def test_build_model_manually():
+    sc = scenario_tools.DeflexScenario(name='my_test', year=2014, debug=True)
+    xls_fn = os.path.join(
+        os.path.dirname(__file__), 'data', 'deflex_2013_de02_test.xls')
+    sc.load_excel(xls_fn)
+    nodes = sc.create_nodes()
+    sc.add_nodes(nodes)
+    sc.create_model()
+    sc.solve(solver='cbc', with_duals=True)
+    eq_(sc.es.results['meta']['scenario']['name'], 'my_test')
+    dump_fn = os.path.join(
+        os.path.expanduser('~'), 'nose_test.deflex')
+    sc.dump_es(dump_fn)
+    sc.restore_es(dump_fn)
+    eq_(sc.meta['scenario']['year'], 2014)
