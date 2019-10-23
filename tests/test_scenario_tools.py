11a5428c1c00d9257a242110cc8d3b6b59fad8c0
uvchik <uvchik.git@posteo.eu>
2019-10-23 14:55:58 +0200

    Revert "Use pytest instead of nose"
    This reverts commit 2b0b315eee5349017708a0bf818c4bdca5464bf8.

--- tests/test_scenario_tools.py
+++ tests/test_scenario_tools.py
@@ -9,26 +9,23 @@ SPDX-License-Identifier: MIT
 __copyright__ = "Uwe Krien <krien@uni-bremen.de>"
 __license__ = "MIT"
 
-
+from nose.tools import assert_raises_regexp
 import os
-import pytest
 from deflex import scenario_tools
 
 
-class TestScenarioTools:
+def test_scenario_building():
+    sc = scenario_tools.DeflexScenario(name='test', year=2014)
+    csv_path = os.path.join(
+        os.path.dirname(__file__), 'data', 'deflex_2014_de21_test_csv')
+    sc.load_csv(csv_path)
+    sc.table2es()
 
-    def test_scenario_building(self):
-        sc = scenario_tools.DeflexScenario(name='test', year=2014)
-        csv_path = os.path.join(
-            os.path.dirname(__file__), 'data', 'deflex_2014_de21_test_csv')
-        sc.load_csv(csv_path)
-        sc.table2es()
-
-    def test_node_dict(self):
-        nc = scenario_tools.NodeDict()
-        nc['g'] = 5
-        nc['h'] = 6
-        msg = ("Key 'g' already exists. Duplicate keys are not allowed in a "
-               "node dictionary.")
-        with pytest.raises(KeyError, match=msg):
-            nc['g'] = 7
+def test_node_dict(self):
+    nc = scenario_tools.NodeDict()
+    nc['g'] = 5
+    nc['h'] = 6
+    msg = ("Key 'g' already exists. Duplicate keys are not allowed in a "
+           "node dictionary.")
+    with assert_raises_regexp(KeyError, msg):
+        nc['g'] = 7
