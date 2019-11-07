084b1e11a1b644431a2df982b12085316fadd811
uvchik <uvchik.git@posteo.eu>
2019-11-07 21:45:23 +0100

    Use most actual reegis version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     install_requires=[
         'oemof >= 0.3.0',
         'pandas >= 0.17.0',
-        'reegis == v0.1.0-rc.8',
+        'reegis == v0.1.0-rc.9',
         'demandlib',
         'workalendar',
         'networkx',
