e86dde2774e8e25e605be0df16cc039ca5fc2e07
uvchik <uvchik.git@posteo.eu>
2019-11-07 11:25:46 +0100

    Remove unreachable code

--- deflex/transmission.py
+++ deflex/transmission.py
@@ -197,13 +197,6 @@ def get_electrical_transmission_renpass(both_directions=False):
         elif cap2 == 0:
             pwr_lines.loc[l, 'capacity'] = cap1
             pwr_lines.loc[l, 'distance'] = dist1
-        elif cap1 == cap2:
-            pwr_lines.loc[l, 'capacity'] = cap1
-            pwr_lines.loc[l, 'distance'] = dist1
-        else:
-            msg = ("The same line ({0}, {1}) has different capacities: {2} "
-                   "!= {3}")
-            raise ValueError(msg.format(a, b, cap1, cap2))
 
     # plot_grid(pwr_lines)
     df = pwr_lines[['capacity', 'distance']]
