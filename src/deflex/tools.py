f5e6d3828b9256a8ff5d902c11964c7ed3ce2b25
uvchik <uvchik.git@posteo.eu>
2021-01-26 14:20:25 +0100

    Test tools

--- src/deflex/tools.py
+++ src/deflex/tools.py
@@ -10,18 +10,10 @@ SPDX-License-Identifier: MIT
 import logging
 import os
 
-try:
-    import requests
-except ModuleNotFoundError:
-    requests = None
+import requests
 
 
 def download(fn, url):
-    if requests is None:
-        raise ModuleNotFoundError(
-            "You cannot download a file without >requests< installed."
-            "Use:\n pip install requests"
-        )
     if not os.path.isfile(fn):
         logging.info(
             "Downloading '{0}' from {1}".format(os.path.basename(fn), url)
@@ -30,3 +22,6 @@ def download(fn, url):
         with open(fn, "wb") as fout:
             fout.write(req.content)
             logging.info("{1} downloaded from {0}.".format(url, fn))
+
+
+
