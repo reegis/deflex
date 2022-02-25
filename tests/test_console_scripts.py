edaddf73cfa58da5492dcf4f63a2e88de289b5d2
uvchik <uvchik.git@posteo.eu>
2022-02-25 12:29:05 +0100

    Add basic test for deflex_result

--- tests/test_console_scripts.py
+++ tests/test_console_scripts.py
@@ -6,3 +6,10 @@ def test_deflex_compute(script_runner):
     ret = script_runner.run("deflex-compute", "--version")
     assert ret.success
     assert ret.stdout == "deflex 0.4.0rc1\n"
+
+
+@pytest.mark.script_launch_mode("subprocess")
+def test_deflex_result(script_runner):
+    ret = script_runner.run("deflex-result", "--version")
+    assert ret.success
+    assert ret.stdout == "deflex 0.4.0rc1\n"
